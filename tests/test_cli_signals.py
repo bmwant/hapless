831a121ec26fde14cab9c824c168f0234e22c1e3
Misha Behersky <bmwant@gmail.com>
2022-05-23 12:08:56 +0200

    Add tests to the signal method :clock4:

--- tests/test_cli_signals.py
+++ tests/test_cli_signals.py
@@ -1,3 +1,4 @@
+import signal
 from unittest.mock import Mock, patch
 
 from hapless import cli
@@ -52,13 +53,32 @@ def test_signal_invocation(get_or_exit_mock, runner):
         signal_mock.assert_called_once_with(hap_mock, 9)
 
 
-def test_signal_invalid_hap():
-    pass
+def test_signal_invalid_hap(runner):
+    with patch.object(runner.hapless, "signal") as signal_mock:
+        result = runner.invoke(cli.cli, ["signal", "invalid-hap", "9"])
+        assert result.exit_code == 1
+        assert "No such hap" in result.stdout
+        signal_mock.assert_not_called()
 
 
-def test_signal_wrong_code():
-    pass
+@patch("hapless.cli.get_or_exit")
+def test_signal_wrong_code(get_or_exit_mock, runner):
+    hap_mock = Mock()
+    get_or_exit_mock.return_value = hap_mock
+    wrong_code = f"{signal.NSIG}"
+    with patch.object(runner.hapless, "signal") as signal_mock:
+        result = runner.invoke(cli.cli, ["signal", "hap-name", wrong_code])
+        assert result.exit_code == 2
+        assert f"{wrong_code} is not a valid signal code" in result.stdout
+        get_or_exit_mock.assert_not_called()
+        signal_mock.assert_not_called()
 
 
-def test_signal_inactive_hap():
-    pass
+@patch("hapless.cli.get_or_exit")
+def test_signal_inactive_hap(get_or_exit_mock, runner):
+    hap_mock = Mock(active=False)
+    get_or_exit_mock.return_value = hap_mock
+    result = runner.invoke(cli.cli, ["signal", "hap-name", "15"])
+    assert result.exit_code == 0
+    assert "Cannot send signal to the inactive hap" in result.stdout
+    get_or_exit_mock.assert_called_once_with("hap-name")
