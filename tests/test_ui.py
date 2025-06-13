c7c96c22c64160a82e9515e9dad4c36fb4ea9b7f
Misha Behersky <bmwant@gmail.com>
2025-06-13 13:43:14 +0200

    Fix issue with long hap id (#51)

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -5,3 +5,13 @@ from hapless.ui import ConsoleUI
 def test_default_formatter_is_table():
     ui = ConsoleUI()
     assert isinstance(ui.default_formatter, TableFormatter)
+
+
+def test_long_hids_are_visible(capsys, hapless):
+    ui = ConsoleUI()
+    long_hid = "9876543210"
+    hap = hapless.create_hap("true", hid=long_hid)
+    ui.stats([hap])
+    captured = capsys.readouterr()
+    assert long_hid in captured.out
+    assert hap.name in captured.out
