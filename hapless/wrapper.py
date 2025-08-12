dd883810d02ae4688615fd92764d55a0f264736f
Misha Behersky <bmwant@gmail.com>
2025-08-12 11:26:36 +0200

    Allow wrapping only unbound haps

--- hapless/wrapper.py
+++ hapless/wrapper.py
@@ -1,13 +1,17 @@
 import click
 
-from hapless.cli_utils import get_or_exit, hap_argument, hapless
+from hapless import Status
+from hapless.cli_utils import console, get_or_exit, hap_argument, hapless
 
 
-@click.group()  # invoke_without_command=True)
+@click.group(invoke_without_command=True)
 @hap_argument
 def wrap_hap(hap_alias: str) -> None:
     print("State is here:", hapless._hapless_dir)
     hap = get_or_exit(hap_alias)
+    if hap.status != Status.UNBOUND:
+        console.error(f"Hap {hap} is already bound to a process")
+        return
     hapless._wrap_subprocess(hap)
 
 
