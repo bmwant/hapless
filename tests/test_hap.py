b4d9d8b93018c37ce0c63df182f69441fc08aeea
Misha Behersky <bmwant@gmail.com>
2025-08-15 13:14:38 +0200

    Enable logging in tests with caplog context

--- tests/test_hap.py
+++ tests/test_hap.py
@@ -1,4 +1,5 @@
 import getpass
+import logging
 import os
 import re
 from io import StringIO
@@ -80,14 +81,15 @@ def test_raw_name(tmp_path):
 
 @pytest.mark.parametrize("redirect_stderr", [True, False])
 def test_correct_redirect_state(tmp_path, caplog, redirect_stderr):
-    hap = Hap(
-        Path(tmp_path),
-        name="hap-name",
-        cmd="doesnotexist",
-        redirect_stderr=redirect_stderr,
-    )
-    assert hap.redirect_stderr is redirect_stderr
-    assert ("stderr will be redirected to stdout" in caplog.text) is redirect_stderr
+    with caplog.at_level(logging.DEBUG, logger="hapless"):
+        hap = Hap(
+            Path(tmp_path),
+            name="hap-name",
+            cmd="doesnotexist",
+            redirect_stderr=redirect_stderr,
+        )
+        assert hap.redirect_stderr is redirect_stderr
+        assert ("stderr will be redirected to stdout" in caplog.text) is redirect_stderr
 
 
 def test_hap_inaccessible(hap: Hap):
