d53f11f4259aa1e099db7830267ebeca951c73a1
Misha Behersky <bmwant@gmail.com>
2025-09-18 12:51:12 +0200

    Add Python 3.13/3.14 support (#64)

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 POETRY_EXEC = "poetry1.8"
-TARGET_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"]
+TARGET_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.14"]
 
 
 @nox.session(python=TARGET_VERSIONS)
