b8c0236ccd90ee0f6d68c1ae41a22aed6cf90aa9
Misha Behersky <bmwant@gmail.com>
2025-10-02 09:57:17 +0200

    Add tests for get_or_exit function

--- tests/test_cli_utils.py
+++ tests/test_cli_utils.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import Mock, PropertyMock, patch
 
 import pytest
 
@@ -16,3 +16,23 @@ def test_get_or_exit_non_existing(hapless_mock, capsys):
 
         captured = capsys.readouterr()
         assert "No such hap: hap-me" in captured.out
+
+
+@patch("hapless.cli_utils.hapless")
+def test_get_or_exit_not_accessible(hapless_mock, capsys):
+    hap_mock = Mock(owner="someone-else")
+    prop_mock = PropertyMock(return_value=False)
+    type(hap_mock).accessible = prop_mock
+    with patch.object(hapless_mock, "get_hap", return_value=hap_mock) as get_hap_mock:
+        with pytest.raises(SystemExit) as e:
+            get_or_exit("hap-not-mine")
+
+        assert e.value.code == 1
+        get_hap_mock.assert_called_once_with("hap-not-mine")
+        prop_mock.assert_called_once_with()
+
+        captured = capsys.readouterr()
+        assert (
+            "Cannot manage hap launched by another user. Owner: someone-else"
+            in captured.out
+        )
